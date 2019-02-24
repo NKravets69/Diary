d8a1a782c860e5aff402b3808eaceb9e75551bb6
Your Name <you@example.com>
2019-02-24 18:22:43 +0100

    My Diary

---
+++ script.js
@@ -0,0 +1,27 @@
+var day = "", //день недели
+  text = "", //текст
+  i = 1, //счетчик
+  arrNotes = [], //массив заметок
+  form = document.dwsForm;
+
+function btnClick() {
+  day = form.days.value;
+  text = form.message.value;
+
+  arrNotes.push(
+    "<tr><td>" +
+      i +
+      "</td><td>Заметка на: " +
+      day.bold() +
+      "</td></tr>" +
+      "<tr><td></td><td>" +
+      text +
+      "<hr></td></tr>"
+  );
+  i++;
+  form.reset();
+}
+function prClick() {
+  document.getElementById("diary").innerHTML =
+    "<table>" + arrNotes.join("") + "</table>";
+}
